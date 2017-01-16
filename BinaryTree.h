555f7220d01e62f9f77b52d544affc12c36e81ac
Aidan Sommerfeld <spacerunner3@hotmail.com>
2017-01-16 17:08:10 -0600

    Create BinaryTree.h
    Binary tree variables and function prototypes

---
+++ BinaryTree.h
@@ -0,0 +1,27 @@
+#ifndef BINARYTREE_H
+#define BINARYTREE_H
+#include "node.h"
+class BT
+{
+public:
+	BT();
+	BT(int nodes);
+	void insertRandomNode();
+	void randomHelper(Node* root, int &depth);
+
+	void addDepth(int nodeDepth);
+	void incrementNodes();
+
+	int getTreeDepth();
+	int getTreeNodes();
+	void removeTree(Node* root);
+	~BT();
+
+	
+private:
+	Node root;
+	int totalTreeDepth;
+	int totalTreeNodes;
+	int maxNodes;
+};
+#endif
