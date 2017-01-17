0e22c4c82a665258e3bce3e83af690569032accc
elementfourthree <queypo3r@uregina.ca>
2017-01-17 10:45:29 -0600

    Adding a BST header
    Base header file. idr which operations were different.

---
+++ binarySearchTree.h
@@ -0,0 +1,16 @@
+/*===================================================
+BinarySearchTree.h
+=====================================================
+
+A header defining a binary search tree class.
+
+Nodes are appended by key. Any new node added to
+the tree is compared against the key of the root:
+left if lower than; right if greater than.
+
+If no node exists where a node travels, it becomes
+appended as the child node; else, the node's key
+is compared against the next node in the branch
+and the process repeats.
+===================================================*/
+
