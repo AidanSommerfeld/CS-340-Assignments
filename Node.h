7d9c03e35fcf4622f8e6fc9641afeba3e884d84c
elementfourthree <queypo3r@uregina.ca>
2017-01-16 20:45:54 -0600

    Update Node.h
    Added comments / formatting.

--- Node.h
+++ Node.h
@@ -1,30 +1,49 @@
 #ifndef NODE_H
 #define NODE_H
+
 class Node
 {
-private:
 
-	Node* left;
-	Node* right;
-	int key;
-	int value;
+	private:
 
-public:
-	Node();
-	Node(int newKey, int newValue);
+		Node* left;
+		Node* right;
 
-	void setKey(int newKey);
-	void setValue(int newValue);
+		int key;		// Determines the branching of a node.
+		int value;		// Arbitrary value container.
 
-	int getKey();
-	int getValue(); 
+	public:
 
-	void setLeft(Node* newNode);    // calls default / init constructor?
-	void setRight(Node* newNode);   // could be omiited if BT class is friend class?
-	Node* getLeft();
-	Node* getRight();
+	// Default & initializing constructors.
 
-	~Node();           //deletes all values in the node
-	                   // Recursively called by BT destructor? NPost-order traversal
+		Node();
+		Node(int newKey, int newValue);
+
+	// Destructor. Should be called recursively by any binary tree destructor.
+
+		~Node();
+
+	// Getter & setter functions.
+
+		void setKey(int newKey);
+		void setValue(int newValue);
+
+		int getKey();
+		int getValue();
+
+	// Initializes branch nodes of a root. 
+	// Note that any branch node is its own root, so this should call a Node() constructor.
+	
+	// Dev note: could we omit these if class BT was a friend class to class Node?
+
+		void setLeft(Node* newNode);    
+		void setRight(Node* newNode);  
+
+	// Returns the address of any branches. These should return some invalid value if uninitialized.
+
+		Node* getLeft();
+		Node* getRight();		         
+	
 };
+
 #endif
