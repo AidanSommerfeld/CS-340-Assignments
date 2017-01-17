cadc0429d1de436267ad92226b4baa6fade2bf2a
elementfourthree <queypo3r@uregina.ca>
2017-01-17 10:43:34 -0600

    Update source.cpp
    Added a project header.

--- source.cpp
+++ source.cpp
@@ -1,25 +1,65 @@
+// ======================================================================
+// Binary Tree Generation
+/* ======================================================================
+// Authors:
+// STNO#(s):
+//                                                                     */
+// =====================================================================
+/* Class:  CS 340
+// For: Assignment #1
+// Date: January 23, 2017                                             */
+// ======================================================================
+/* Purpose: The main executable file for a process that generates
+//			several types of binary trees to experimentally 
+//			average and compare their depth. 
+//
+// Algorithm / Process:
+//
+//			Creates x dynamically-allocated trees with y nodes. 
+//			During program operation, two counters are given to track 
+//			the number of total nodes created in a process, as well as
+//			the cumulative depth of all nodes that have been allocated.
+//
+//			These counters are fields in the binary tree classes. Before
+//			a new tree is generated, these counters are added to an overall
+//			cumulative counter before the current tree is de-allocated --
+//			then a new tree is generated until the target number of trees 
+//			is built.
+//
+// Major Variables and Constants:
+//
+// ======================================================================*/
+
 #include <iostream>
-#include "BinaryTree.h"
-#include "node.h"
-#include <stdlib.h>
+#include <cstdlib>
 #include <ctime>
+
+#include "binaryTree.h"
+#include "binarySearchTree.h"
+#include "node.h"
+
 using namespace std;
 
 int main()
 {
 	srand(time(0));
+
 	float totalNodeCount = 0;
 	float totalDepthCount = 0;
 	float average = 0;
+
+	// Counters for the numbers of trees / nodes built per process.
+
 	int treeNumber;
 	int nodeNumber;
 	int frequency;
 
-	cout << "How many trees would you like to test: ";
+	cout << "Enter the number of binary trees to generate: ";
 	cin >> treeNumber;
-	cout << "How many nodes will be in each tree: ";
+	cout << "Enter the number of nodes per tree: ";
 	cin >> nodeNumber;
-	cout << "please enter a print frequency: ";
+	cout << "Enter a print frequency." << '\n'
+		 << "(This is a visual counter that prints a dot indicating n trees have been built.): ";
 	cin >> frequency;
 	nodeNumber--;
 
@@ -32,22 +72,23 @@ int main()
 		{
 			tree->insertRandomNode();
 		}
+
 		totalNodeCount += tree->getTreeNodes();
 		totalDepthCount += tree->getTreeDepth();
 		delete tree;
+
 		if (i % frequency == 0)
-		{
 			cout << '.';
-		}
-		if (i % (frequency * 10) == 0 && i != 0 )
-		{
+		if (i % (frequency * 10) == 0 && i != 0)
 			cout << endl;
-		}
+
 	}
 	cout << endl;
 	average = totalDepthCount / totalNodeCount;
 
 	cout << "The average depth of this tree is: " << average << endl;
-	cout << "The total number of nodes was: " << fixed << totalNodeCount << " The total depth was: "  << fixed << totalDepthCount << endl;
+	cout << "The total number of nodes built was: " << fixed << totalNodeCount << '\n'
+		 << "The total depth was: " << fixed << totalDepthCount << endl;
+
 	return 0;
 }
