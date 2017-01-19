b8b035c30b818bb00d4a87997780cba9832adf71
elementfourthree <queypo3r@uregina.ca>
2017-01-18 22:27:37 -0600

    Update binarySearchTree.h
    Barring some major fuckup this should be the final version of the BST header

--- binarySearchTree.h
+++ binarySearchTree.h
@@ -12,5 +12,37 @@ If no node exists where a node travels, it becomes
 appended as the child node; else, the node's key
 is compared against the next node in the branch
 and the process repeats.
+
+BST is a derived class of BT and provides
+an overload for node insertion.
 ===================================================*/
 
+#ifndef BIN_SEARCH_H
+#define BIN_SEARCH_H
+
+#include "node.h"
+#include "binaryTree.h"
+
+#include <iostream>
+#include <cstdlib>
+
+class BST : public BT
+{
+	public: 
+
+		BST();
+		BST(int nodes);
+		~BST();
+
+		void insertNode();
+		void insertHelper(Node* root, int& depth);
+
+		// These functions are called by the 
+		// BST insertion algorithm.
+
+		void find();
+		void findMin();
+		void findMax();
+};
+
+#endif
