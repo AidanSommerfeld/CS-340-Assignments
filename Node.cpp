afab4bfb5ce532bcf4f93ce967ac3404de9ec883
elementfourthree <queypo3r@uregina.ca>
2017-01-19 03:00:24 -0600

    Comments / sorting
    etc

--- Node.cpp
+++ Node.cpp
@@ -1,81 +1,73 @@
 #include "node.h"
+
 using namespace std;
 
-//CONSTRUCTORS
+// ----------------------------------------
+// Constructors for nodes.
+
 Node::Node()
+/* Default constructor. */
 {
 	key = 0;
 	value = 0;
+
 	left = nullptr;
 	right = nullptr;
+
 	leftHeight = 0;
 	rightHeight = 0;
 }
+
 Node::Node(int newKey, int newValue)
+/* Initializing constructor. */
 {
 	key = newKey;
 	value = newValue;
+	
 	left = nullptr;
 	right = nullptr;
+	
 	leftHeight = 0;
 	rightHeight = 0;
 }
-//SET INTERNAL VALUES//
+
+// ----------------------------------------
+// Internal value setters.
 void Node::setKey(int newKey)
 {
 	key = newKey;
 }
+
 void Node::setValue(int newValue)
 {
 	value = newValue;
 }
-//GET INTERNAL VALUES//
+
+// ----------------------------------------
+// Functions returning private data fields.
 int Node::getKey()
 {
 	return key;
 }
+
 int Node::getValue()
 {
 	return value;
 }
-//AVL ONLY FUNCTIONS//////////////////////////
 
-//GET HEIGHTS//
-int Node::getLeftHeight()
-{
-	return leftHeight;
-}
-int Node::getRightHeight()
-{
-	return rightHeight;
-}
-void Node::setLeftHeight(int num)
-{
-	leftHeight = num;
-}
-void Node::setRightHeight(int num)
-{
-	rightHeight = num;
-}
-void Node::incrementLeftHeight()
-{
-	leftHeight++;
-}
-void Node::incrementRightHeight()
-{
-	rightHeight++;
-}
-//////////////////////////////////////////////
-//SET CHILDREN//
+// ----------------------------------------
+// Subtree operations. 
 void Node::setLeft(Node* newNode)   // calls default / init constructor?
 {
 	left = newNode;
 }
+
 void Node::setRight(Node* newNode)  // could be omiited if BT class is friend class?
 {
 	right = newNode;
 }
-//GET CHILDREN//
+
+
 Node* Node::getLeft()
 {
 	return left;
@@ -85,7 +77,43 @@ Node* Node::getRight()
 {
 	return right;
 }
+
+// ----------------------------------------
+// AVL tree-specific operations.
+
+// Height operations.
+int Node::getLeftHeight()
+{
+	return leftHeight;
+}
+
+int Node::getRightHeight()
+{
+	return rightHeight;
+}
+
+void Node::setLeftHeight(int num)
+{
+	leftHeight = num;
+}
+
+void Node::setRightHeight(int num)
+{
+	rightHeight = num;
+}
+
+void Node::incrementLeftHeight()
+{
+	leftHeight++;
+}
+
+void Node::incrementRightHeight()
+{
+	rightHeight++;
+}
+
 void Node::operator= (Node* node2)
+/* An operator overload to expedite AVL rotations. */
 {
 	left = node2->getLeft();
 	right = node2->getRight();
@@ -94,8 +122,9 @@ void Node::operator= (Node* node2)
 	leftHeight = node2->getLeftHeight();
 	rightHeight = node2->getLeftHeight();
 }
-//DESTRUCTOR//
-Node::~Node()           //deletes all values in the node
-{				        // Recursively called by BT destructor? NPost-order traversal
 
+Node::~Node()   
+/* What are we doing with this? 
+   It seems like the BT destructor is doing the job... */
+{			        
 }
