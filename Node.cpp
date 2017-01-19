f38058b798b92c7dd03270588d44fdfaca148d7b
Aidan Sommerfeld <spacerunner3@hotmail.com>
2017-01-18 22:36:58 -0600

    Update Node.cpp
    added = operator and a few variables for avl trees

--- Node.cpp
+++ Node.cpp
@@ -1,13 +1,15 @@
 #include "node.h"
 using namespace std;
 
-
+//CONSTRUCTORS
 Node::Node()
 {
 	key = 0;
 	value = 0;
 	left = nullptr;
 	right = nullptr;
+	leftHeight = 0;
+	rightHeight = 0;
 }
 Node::Node(int newKey, int newValue)
 {
@@ -15,8 +17,10 @@ Node::Node(int newKey, int newValue)
 	value = newValue;
 	left = nullptr;
 	right = nullptr;
+	leftHeight = 0;
+	rightHeight = 0;
 }
-
+//SET INTERNAL VALUES//
 void Node::setKey(int newKey)
 {
 	key = newKey;
@@ -25,7 +29,7 @@ void Node::setValue(int newValue)
 {
 	value = newValue;
 }
-
+//GET INTERNAL VALUES//
 int Node::getKey()
 {
 	return key;
@@ -34,7 +38,27 @@ int Node::getValue()
 {
 	return value;
 }
+//AVL ONLY FUNCTIONS//////////////////////////
 
+//GET HEIGHTS//
+int Node::getLeftHeight()
+{
+	return leftHeight;
+}
+int Node::getRightHeight()
+{
+	return rightHeight;
+}
+void Node::incrementLeftHeight()
+{
+	leftHeight++;
+}
+void Node::incrementRightHeight()
+{
+	rightHeight++;
+}
+//////////////////////////////////////////////
+//SET CHILDREN//
 void Node::setLeft(Node* newNode)   // calls default / init constructor?
 {
 	left = newNode;
@@ -43,7 +67,7 @@ void Node::setRight(Node* newNode)  // could be omiited if BT class is friend cl
 {
 	right = newNode;
 }
-
+//GET CHILDREN//
 Node* Node::getLeft()
 {
 	return left;
@@ -53,7 +77,16 @@ Node* Node::getRight()
 {
 	return right;
 }
-
+void Node::operator= (Node* node2)
+{
+	left = node2->getLeft();
+	right = node2->getRight();
+	key = node2->getKey();
+	value = node2->getValue();
+	leftHeight = node2->getLeftHeight();
+	rightHeight = node2->getLeftHeight();
+}
+//DESTRUCTOR//
 Node::~Node()           //deletes all values in the node
 {				        // Recursively called by BT destructor? NPost-order traversal
 
