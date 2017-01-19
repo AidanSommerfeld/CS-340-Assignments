4aa2a328d14451647e7ee084221220d62f178565
elementfourthree <queypo3r@uregina.ca>
2017-01-19 02:08:14 -0600

    comments + implementation of print function
    I'm not sure if BT had its own printer function but for the sake of argument I've added them to the base BT class and header. Comments, etc.

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -1,43 +1,35 @@
-#include "BinaryTree.h"
 #include <iostream>
 #include <ctime>
-#include <stdlib.h>
+#include <cstdlib>
+
+#include "BinaryTree.h"
+
 using namespace std;
 
-void BT::printHelper(Node* root)
-{
-	if (root == nullptr)
-	{
-		return;
-	}
-	printHelper(root->getLeft());
-	printHelper(root->getRight());
-
-	cout << root->getKey() << endl;
-}
-void BT::printTree()
-{
-	cout << "Depth: " << totalTreeDepth << " Node number:" << totalTreeNodes << endl;
-	printHelper(root);
-}
 BT::BT()
+/* Default constructor. */
 {
-	cout << "begin" << endl;
 	root->setKey(rand() % 10);
-	cout << "Set a key" << endl;
+	cout << "Key set for root node." << endl;
 	root->setValue(rand() % 10000);
-	cout << "Set a value" << endl;
+	cout << "Value set for root node." << endl;
 	totalTreeDepth = 0;
 	totalTreeNodes = 1;
-	maxNodes = 100000;	               //arbitrary number since the user is obligated to enter a number this value should never be hit (unless of course the user chooses 1)
+
+	// This is arbitrary. Program design calls for the user to enter a value anyway,
+	// and besides for most purposes the initializing constructor will be used.
+	maxNodes = 100000;
 }
 
 BT::BT(int nodes)
+/* Initializing constructor, given a user input.*/
 {
 	root->setKey(rand() % 10);
 	root->setValue(rand() % 10000);
+
 	totalTreeDepth = 0;
 	totalTreeNodes = 1;
+
 	maxNodes = nodes;
 }
 
@@ -61,72 +53,120 @@ int BT::getTreeNodes()
 	return totalTreeNodes;
 }
 
-void BT::randomHelper(Node* root, int& depth)
-{
-	depth++;
-
-	int decide = rand() % 2;
-
-	if (decide == 0)
-	{
-		if (root->getLeft() == nullptr)
-		{
-			Node* temp = new Node;
-			temp->setKey(rand() % 10);
-			temp->setValue(rand() % 10000);
-			temp->setLeft(nullptr);
-			temp->setRight(nullptr);
-			root->setLeft(temp);
-			addDepth(depth);
-		}
-		else
-		{
-			randomHelper(root->getLeft(), depth);
-		}
-	}
-	else
-	{
-		if (root->getRight() == nullptr)
-		{
-			Node* temp = new Node;
-			temp->setKey(rand() % 10);
-			temp->setValue(rand() % 10000);
-			temp->setLeft(nullptr);
-			temp->setRight(nullptr);
-			root->setRight(temp);
-			addDepth(depth);
-		}
-		else
-		{
-			randomHelper(root->getRight(), depth);
-		}
-	}
-}
-
-void BT::insertRandomNode()
+void BT::insertNode()
+/* Adds nodes to the tree until target # of nodes is reached. */
 {
 	if (maxNodes > totalTreeNodes)
 	{
 		int depth = 0;
-		randomHelper(root, depth);
+		insertHelper(root, depth);
 		incrementNodes();
 	}
 }
 
-void BT::removeTree(Node* root)
+void BT::insertHelper(Node* root, int& depth)
+/* Recursive insertion helper of insertNode(). */
+{
+	depth++; // ?? May need to look into this.
+
+	// To reiterate: binary trees have no rules on adding nodes,
+	// so here we simply randomize the choice. 
+	// 0 adds the node to the left subtree; 1 to the right.
+
+	int decide = rand() % 2;
+
+	if (decide == 0) // Goes left.
+	{
+		if (root->getLeft() == nullptr)	// If no subtree currently exists...
+		{
+			Node* temp = new Node;
+
+			// Binary trees don't care about keys or values,
+			// so these are arbitrarily set here and make no
+			// difference in the insertion process.
+
+			temp->setKey(rand() % 10);
+			temp->setValue(rand() % 10000);
+
+			// Initializes the new node as a subtree of its parent.
+
+			temp->setLeft(nullptr);
+			temp->setRight(nullptr);
+			root->setLeft(temp);
+
+			// Returns the depth of the current node to the depth accumulator.
+
+			addDepth(depth);
+		}        
+		else                            
+		{	
+			// Otherwise recursively call the function and 
+			// branch down to the existing node.
+			// Repeat until an open subtree is found.
+			insertHelper(root->getLeft(), depth);
+		}
+	}
+	else // Goes right. Logic is the same; see above.
+	{
+		if (root->getRight() == nullptr)
+		{
+			Node* temp = new Node;
+
+			temp->setKey(rand() % 10);
+			temp->setValue(rand() % 10000);
+
+			temp->setLeft(nullptr);
+			temp->setRight(nullptr);
+			root->setRight(temp);
+
+			addDepth(depth);
+		}
+		else
+		{
+			insertHelper(root->getRight(), depth);
+		}
+	}
+}
+
+void BT::removeNode(Node* root)
+/* Post-order deletion. Root MUST go last. */
 {
 	if (root == nullptr)
-	{
 		return;
-	}
-	removeTree(root->getLeft());
-	removeTree(root->getRight());
+
+	// As a note: if we deleted any non-root node
+	// this would delete all of its children underneath.
+	// In practice only the destructor calls this, though.
+
+	removeNode(root->getLeft());
+	removeNode(root->getRight());
 	delete root;
 
 }
 
+void BT::printTree()
+{
+	cout << "Depth: " << totalTreeDepth << '\n'
+		<< "Node number:" << totalTreeNodes << endl;
+
+	printHelper(root);
+}
+
+void BT::printHelper(Node* root)
+/* Post-order traversal. Prints from the bottom up. */
+{
+	if (root == nullptr)
+		return;
+
+	printHelper(root->getLeft());
+	printHelper(root->getRight());
+
+	cout << root->getKey() << endl;
+}
+
+
 BT::~BT()
 {
-	removeTree(root);
+	removeNode(root);
 	root = nullptr;
 }
