51bb0bcb0869f39d5794d2cbf99de6b1b5706a34
elementfourthree <queypo3r@uregina.ca>
2017-01-23 03:30:59 -0600

    Update Node.h
    leftheight rightheight are outdated

--- Node.h
+++ Node.h
@@ -2,14 +2,14 @@
 Node.h
 =====================================================
 
-A header defining a class for nodes that 
+A header defining a class for nodes that
 compose a binary tree.
 
-All binary tree headers will refer to this 
+All binary tree headers will refer to this
 class definition header.
 
-class Node is a recyclable class. While 
-binary trees have no need for the key 
+class Node is a recyclable class. While
+binary trees have no need for the key
 and value fields, binary search trees and AVL
 trees are heavily reliant on these fields
 for rebalancing.
@@ -21,57 +21,55 @@ for rebalancing.
 class Node
 {
 
-	private:
+private:
 
-		Node* left;
-		Node* right;
+	Node* left;
+	Node* right;
 
-		int key;		// Determines the branching of a node.
-		int value;		// Arbitrary value container.
-	
-		int leftHeight;         // NOTE:
-		int rightHeight;        // These fields are used only by AVL trees.
+	int key;		// Determines the branching of a node.
+	int value;		// Arbitrary value container.
+	int height;
 
-	public:
+public:
 
 	// Default & initializing constructors.
 
-		Node();
-		Node(int newKey, int newValue);
+	Node();
+	Node(int newKey, int newValue);
 
 	// Destructor. Should be called recursively by any binary tree destructor.
 
-		~Node();
+	~Node();
 
 	// Getter & setter functions.
 
-		void setKey(int newKey);
-		void setValue(int newValue);
+	void setKey(int newKey);
+	void setValue(int newValue);
 
-		int getKey();
-		int getValue();
-	
-	// AVL-specific functions.
-		int getHeight();
-		void setHeight(int num);
-		void incrementHeight();
-	// An operator overload for AVL rotations.
-		void operator= (Node* node2);
+	int getKey();
+	int getValue();
+
+	//AVL ONLY FUNCTIONS//
+	int getHeight();
+	void setHeight(int num);
+	void incrementHeight();
 
 	// Initializes branch nodes of a root. 
 	// Note that any branch node is its own root, so this should call a Node() constructor.
-	
+
 	// Dev note: could we omit these if class BT was a friend class to class Node?
 	// Waste of time, probably.
 
-		void setLeft(Node* newNode);    
-		void setRight(Node* newNode);  		
-		
+	void setLeft(Node* newNode);
+	void setRight(Node* newNode);
+
 	// Returns the address of any branches. These should return some invalid value if uninitialized.
 
-		Node* getLeft();
-		Node* getRight();
-	
+	Node* getLeft();
+	Node* getRight();
+
+	// An operator overload for AVL rotations.
+	void operator= (Node* node2);
 };
 
 #endif
