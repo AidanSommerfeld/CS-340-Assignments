f16ce3a78d205e9652b44f21d90d6a82ecc3d903
elementfourthree <queypo3r@uregina.ca>
2017-01-23 08:55:31 -0600

    Update BinaryTree.h

--- BinaryTree.h
+++ BinaryTree.h
@@ -19,64 +19,66 @@ Classes BST and AVL are derived from this class.
 
 class BT
 {
-	// So class BST and AVL can inherit these.
 	
-	protected:
+protected: 
 
-		// Roots need to be explicitly dynamic here so any tree instance
-		// can likewise be de-allocated without issue.
+	// Member fields are protected, not private, 
+	// so that classes AVL and BST can inherit them.
 
-		Node* root = new Node;
+	// Roots need to be explicitly dynamic here so any tree instance
+	// can likewise be de-allocated without issue.
 
-		// The following values are used for experimental statistic verification.
+	Node *root = new Node;
 
-		// totalTreeDepth is the sum of the depth values assigned to each and every node.
-		// totalTreeNodes is the number of nodes currently being allocated for a tree.
-		// Hence, the average depth of a tree is taken as depth / nodes.
+	// The following values are used for experimental statistic verification.
 
-		// maxNodes defines the maximum number of nodes to be built for a tree
-		// before it is de-allocated and a new tree is generated. 
+	// totalTreeDepth is the sum of the depth values assigned to each and every node.
+	// totalTreeNodes is the number of nodes currently being allocated for a tree.
+	// Hence, the average depth of a tree is taken as depth / nodes.
+
+	// maxNodes defines the maximum number of nodes to be built for a tree
+	// before it is de-allocated and a new tree is generated. 
 		
-		// The average depth of a tree is added to a running 
-		// total in the main function before it is de-allocated
-		// and a new tree is rebuilt.
+	// The average depth of a tree is added to a running 
+	// total in the main function before it is de-allocated
+	// and a new tree is rebuilt.
 
-		int totalTreeDepth;
-		int totalTreeNodes;
-		int maxNodes;
+	int totalTreeDepth;
+	int totalTreeNodes;
+	int maxNodes;
 
-	public:
+public:
 
-	// Constructors, destructor.
+	// Constructors; destructor.
 
-		BT();
-		BT(int nodes);
+	BT();
+	BT(int nodes);
 
-		~BT();
+	~BT();
 
 	// Node insertion. Has a recursive helper function.
 	
-		void insertNode();
-		void insertHelper(Node* root, int &depth);
+	void insertNode();
+	void insertHelper(Node *root, int &depth);
 
 	// Functions to calculate or return tree statistics.
 	
 	// addDepth adds the depth of a node to the running total.
 
-		void addDepth(int nodeDepth);
+	void addDepth(int nodeDepth);
 
-		void printHelper(Node* root);
-		void printTree();
+	void printTree();
+	void printHelper(Node *root);
 	
-		int calculateHelper(Node* root, int depth);
-		void calculateDepth();
+	void calculateDepth();
+	int calculateHelper(Node *root, int depth);
 	
-		void incrementNodes(); 
+	void incrementNodes(); 
 
-		int getTreeDepth();
-		int getTreeNodes();
+	int getTreeDepth();
+	int getTreeNodes();
 
-		void removeNode(Node* root);		
+	void removeTree(Node *root);		
 	
 };
 
