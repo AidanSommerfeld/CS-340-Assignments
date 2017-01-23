f1e4b259ee9a56d423be7695db37f6d111692886
elementfourthree <queypo3r@uregina.ca>
2017-01-23 09:00:37 -0600

    Update AVLTree.h

--- AVLTree.h
+++ AVLTree.h
@@ -1,51 +1,75 @@
+/*===================================================
+AVLTree.h
+=====================================================
+
+A header defining an AVL tree class.
+
+Nodes are appended by key in the same method as 
+a binary search tree.
+
+The identifying property of an AVL tree is that
+it maintains no more than a height difference
+of 1 between left and right subtrees, using
+"rotations" to retain this after nodes are added.
+
+AVL is a derived class of BT and provides
+an overload for node insertion.
+===================================================*/
+
 #ifndef AVLTREE_H
 #define AVLTREE_H
-#include "node.h"
+
+#include "Node.h"
+#include "BinaryTree.h"
+
 #include <iostream>
-#include <stdlib.h>
-using namespace std;
+#include <cstdlib>
 
-class AVL
+
+class AVL : public BT
 {
-private:
-	int maxNodes;
 
-	Node* root = new Node;
-	int totalTreeDepth;
-	int totalTreeNodes;
-	
 public:
+
 	AVL();
 	AVL(int nodes);
 	~AVL();
 
+	// Searches the tree for a specified key.
+	
+	bool searchTree(int num);
+	bool searchHelper(Node *root, bool &found, int num);
+
+	// Used for balancing / insertion.
+
+	int max(int a, int b);
+
+	void insertNode();
+	void insertHelper(Node *root, int &depth, int keyValue);
+
+	// Functions for AVL rotations.
+
+	Node *RR(Node *original);
+	Node *LL(Node *original);
+	Node *LR(Node *original);
+	Node *RL(Node *original);
+
+	/*The following functions are inherited from
+	class BT with no overrides or overloads:
+
 	void addDepth(int nodeDepth);
 	void incrementNodes();
 
-	
-	int calculateHelper(Node* root, int depth);
 	void calculateDepth();
+	int calculateHelper(Node *root, int depth);
 
-	void printHelper(Node* root);
 	void printTree();
-
-	bool searchHelper(Node*root, bool& found, int num);
-	bool searchTree(int num);
+	void printHelper(Node *root);
 
 	int getTreeDepth();
 	int getTreeNodes();
 
-	int max(int a, int b);
-
-	void insertRandomNode();
-	void randomHelper(Node* root, int &depth, int keyValue);
-
-	Node* RR(Node* original);
-	Node* LL(Node* original);
-	Node* LR(Node* original);
-	Node* RL(Node* original);
-
-
-	void removeTree(Node* root);
+	void removeTree(Node *root);
+	*/
 };
 #endif
