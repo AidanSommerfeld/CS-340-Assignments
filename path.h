4a55f9c0b302e59d6b361dd081e4f0d942392ad7
Aidan Sommerfeld <spacerunner3@hotmail.com>
2017-02-10 13:34:30 -0600

    working version
    FUK

--- path.h
+++ path.h
@@ -2,6 +2,11 @@
 #define PATH_H
 #include <iostream>
 using namespace std;
+
+const int FLOYD_SIZE = 8;
+const int HIGH_VALUE = 9999;
+const int VALUE_NOT_FOUND = 10000;
+
 struct Leader;
 struct Follower;
 
@@ -24,8 +29,8 @@ class Dijkstras
 {
 public:
 	Dijkstras::Dijkstras();
-	void Dijkstras::initialization(Leader a[], int& startVertex);
-	void Dijkstras::search(Leader a[], int& startVertex);
+	void Dijkstras::initialization(Leader a[]);
+	void Dijkstras::search(Leader a[], int startVertex);
 	int Dijkstras::findNextVertex(Leader a[]);
 	void Dijkstras::paths(Leader a[]);
 private:
@@ -38,14 +43,14 @@ class Floyds
 {
 public:
 	Floyds::Floyds();
-	void Floyds::initialization(int d[][7],int v[][7],int n);
-	void Floyds::search(int d[][7], int v[][7], int n);
-	void Floyds::paths(int d[][7], int v[][7], int i, int j);
+	void Floyds::initialization(int d[][FLOYD_SIZE], int v[][FLOYD_SIZE], int n);
+	void Floyds::search(int d[][FLOYD_SIZE], int v[][FLOYD_SIZE], int n);
+	void Floyds::paths(int d[][FLOYD_SIZE], int v[][FLOYD_SIZE], int i, int j);
 private:
-	int d[7][7];
-	int v[7][7];
-	int n;
-	int i;
-	int j;
+	int d[FLOYD_SIZE][FLOYD_SIZE];
+	int v[FLOYD_SIZE][FLOYD_SIZE];
+	int n = FLOYD_SIZE;
+	int i = 1;
+	int j = 1;
 };
 #endif
